7e650f98e1193294d3efe79142f3f3302e4e68b7
Nadine Ben Boina <119669536+NdnBnBn@users.noreply.github.com>
2024-01-25 14:47:52 +0100

    Add files via upload

---
+++ biolqm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Script to generate partial permissive model
+
+java -jar /home/nadine/bioLQM/target/bioLQM-0.8-SNAPSHOT.jar $1 -m mp:"$2" $3/$4"_mp.bnet"
+
+
---
+++ partial_most_permissve.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import subprocess as sp
+import os
+
+biolqm = "/home/nadine/Documents/3.Scripts/biolqm.sh"# Biolqm command
+model = "/home/nadine/Documents/1.Model&Simulations/2.Leonard_Model/Model/PMP/ASYN.bnet" # Location of the BN model
+path = "/home/nadine/Documents/1.Model&Simulations/2.Leonard_Model/Model" # Path of the resulting J mp model
+
+
+file_path = "/home/nadine/Documents/1.Model&Simulations/2.Leonard_Model/ML_two_pMP_comb.txt"
+with open(file_path) as f:
+    J = [line.strip() for line in f] # List of sets J¨
+
+# Split names to get nodes
+nodes = [n.split("_") for n in J] # Nodes in each set
+
+# Run subprocess for each set of nodes and names
+for n, j in zip(nodes, J):
+    sp.run([biolqm, model, '  '.join(n), path, j])
